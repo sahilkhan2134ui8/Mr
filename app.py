9741369f8bc9219d571588ebced64df3d2fc0bb9
sahilkhan2134ui8 <152850590+sahilkhan2134ui8@users.noreply.github.com>
2025-02-13 12:08:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
